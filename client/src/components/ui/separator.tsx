1a625b1cc8b1d273ce74e6ca5f8cdc52e5f88b54
Alexis Nde <alexis.nde@pditechnologies.com>
2026-06-09 14:10:44 -0500

    first commit

--- client/src/components/ui/separator.tsx
+++ client/src/components/ui/separator.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
-
 import { cn } from "@/lib/utils";
 
 function Separator({
@@ -8,14 +6,17 @@ function Separator({
   orientation = "horizontal",
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: React.ComponentProps<"div"> & {
+  orientation?: "horizontal" | "vertical";
+  decorative?: boolean;
+}) {
   return (
-    <SeparatorPrimitive.Root
-      data-slot="separator"
-      decorative={decorative}
-      orientation={orientation}
+    <div
+      role={decorative ? "none" : "separator"}
+      aria-orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        "shrink-0 bg-border",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className
       )}
       {...props}
